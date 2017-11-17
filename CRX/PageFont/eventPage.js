1e15c8b97ebb337f03612cae35d7841ee5fae8cf
Matheesan <matheesanm@esna.com>
2017-11-17 14:11:32 -0500

    finished crx tutorials

---
+++ CRX/PageFont/eventPage.js
@@ -0,0 +1,8 @@
+chrome.runtime.onMessage.addListener(function(request,sender,senderResponse){
+    if(request.todo == "showPageAction"){
+        chrome.tabs.query({active: true,currentWindow:true}, function(tabs){
+        chrome.pageAction.show(tabs[0].id);
+        })
+    }
+})
+
