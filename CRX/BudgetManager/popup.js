560d30ba013f4f27c2156939c9092317cd4b987c
matheesan manokaran <mathee25@hotmail.com>
2017-11-16 22:11:30 -0500

    made changes to budgetM

--- CRX/BudgetManager/popup.js
+++ CRX/BudgetManager/popup.js
@@ -1,9 +1,10 @@
 $(function(){
-    chrome.storage.sync.get('total',function(budget){
+    chrome.storage.sync.get(['total','limit'],function(budget){
     $('#total').text(budget.total);
+    $('#limit').text(budget.limit);
         })
     $('#spendAmount').click(function(){
-        chrome.storage.sync.get('total', function(budget){
+        chrome.storage.sync.get(['total','limit'], function(budget){
             var newTotal=0;
             if (budget.total){
                 newTotal += parseInt(budget.total);
@@ -13,7 +14,17 @@ $(function(){
                 newTotal += parseInt(amount);
             }
             
-            chrome.storage.sync.set({'total': newTotal});
+            chrome.storage.sync.set({'total': newTotal}, function(){
+                if(amount && newTotal >= budget.limit){
+                    var notifOptions = {
+                        type: 'basic',
+                        iconUrl: 'icon48.png',
+                        title: 'Limit Reached',
+                        message: "Uh Oh, Looks like you've reached your Limit!"
+                    };
+                    chrome.notifications.create('limitNotif', notifOptions);
+                }
+            });
             $('#total').text(newTotal);
             $('#amount').val('');
         });                    
